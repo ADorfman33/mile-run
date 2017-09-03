63550ca0dc3601fae8032aeab3c4932354804734
Siddhardha Moparthi <siddhardha.moparthi@uconn.edu>
2017-09-03 18:20:28 -0400

    started creating runner class

--- js/Runner.js
+++ js/Runner.js
@@ -1,5 +1,10 @@
 function Runner(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+
+  this.times = {};
+}
+
+Runner.prototype.addTime = function(date, time) {
   
-
-
 }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,48 @@
 $(function() {
   console.log("JS Linked");
+  $('.modal').modal();
+
+  // Setting length of time header based on number of time entries
+  let numTimes = 3;
+  let $times = $("<th colspan=" + numTimes + ">Times</th>");
+  $('#header-times').append($times);
+
+  // Triggering modal to add player
+  $('#add-runner-btn').click(function() {
+    $('#modal1').modal('open');
+  });
+
+  $('#add-runner').click(function(e) {
+    e.preventDefault();
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let newRunner;
+  });
 
 
+  // Creating sample data and adding it to table
+  let $data3 = $(`
+    <tr>
+      <td>Andrew</td>
+      <td>Dorfman</td>
+      <td>4:50</td>
+      <td>6:00</td>
+      <td>8:00</td>
+      <td>4:50</td>
+      <td>6:00</td>
+      <td>8:00</td>
+    </tr>
+  `);
 
+  $('#runner-data').append($data3);
 
 
 
 
 });
+
+function normalize(date, time) {
+  let regex = /(.*)\/(.*)/g;
+
+
+}
