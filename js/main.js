c809d44d4558cfa894415cc094a244de55289c60
Andrew Dorfman <ADorfman@udel.edun>
2017-09-04 11:37:31 -0400

    added plus button. Changed logic for colspan. Added rowspan. No need for variables to keep track of length now

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(function() {
   $('.modal').modal();
 
   // Setting length of time header based on number of time entries
-  let numTimes = 3;
-  let $times = $("<th colspan=" + numTimes + ">Times</th>");
+  var numTimes = 3;
+  var $times = $("<th colspan=" + numTimes + ">Times</th>");
   $('#header-times').append($times);
 
   // Triggering modal to add player
@@ -14,33 +14,36 @@ $(function() {
 
   $('#add-runner').click(function(e) {
     e.preventDefault();
-    let firstName = $('#first-name').val();
-    let lastName = $('#last-name').val();
-    let newRunner;
+    var firstName = $('#first-name').val();
+    var lastName = $('#last-name').val();
+    var newRunner;
   });
 
   // Creating sample data and adding it to table
-  let $data3 = $(`
+  var $data3 = $(`
     <tr>
       <td>Andrew</td>
       <td>Dorfman</td>
       <td>4:50</td>
       <td>6:00</td>
       <td>8:00</td>
+      <td><i class="add-time-btn material-icons">add_circle</i></td>
       <td>4:50</td>
       <td>6:00</td>
-      <td>8:00</td>
+      <td>2:00</td>
     </tr>
   `);
 
   $('#runner-data').append($data3);
 
 
-  [date, time] = normalize("9/20", "2:30");
-  let dateN = date[0] + "/" + date[1];
-  let timeN = time[0] + ":" + time[1];
 
-  let runner3 = new Runner("Jon", "Doe");
+
+  [date, time] = normalize("9/20", "2:30");
+  var dateN = date[0] + "/" + date[1];
+  var timeN = time[0] + ":" + time[1];
+
+  var runner3 = new Runner("Jon", "Doe");
   runner3.addTime(dateN, timeN);
 
 
@@ -48,23 +51,23 @@ $(function() {
 });
 
 function normalize(date, time) {
-  let regexDate = /(.*)\/(.*)/g;
-  let normalizedData = [];
-  let m;
+  var regexDate = /(.*)\/(.*)/g;
+  var normalizedData = [];
+  var m;
 
-  while((m = regexDate.exec(date)) !== null) {
+  while ((m = regexDate.exec(date)) !== null) {
     // This is necessary to avoid infinite loops with zero-width matches
-    if(m.index === regexDate.lastIndex) {
-        regexDate.lastIndex++;
+    if (m.index === regexDate.lastIndex) {
+      regexDate.lastIndex++;
     }
-    let month = Number(m[1]);
-    let day = Number(m[2]);
-    if(month < 10) {
+    var month = Number(m[1]);
+    var day = Number(m[2]);
+    if (month < 10) {
       month = "0" + month;
     } else {
       month = month.toString();
     }
-    if(day < 10) {
+    if (day < 10) {
       day = "0" + day;
     } else {
       day = day.toString();
@@ -72,22 +75,22 @@ function normalize(date, time) {
     normalizedData.push([month, day]);
   }
 
-  let regexTime = /(.*):(.*)/g;
-  let n;
+  var regexTime = /(.*):(.*)/g;
+  var n;
 
-  while((n = regexTime.exec(time)) !== null) {
+  while ((n = regexTime.exec(time)) !== null) {
     // This is necessary to avoid infinite loops with zero-width matches
-    if(n.index === regexTime.lastIndex) {
-        regexTime.lastIndex++;
+    if (n.index === regexTime.lastIndex) {
+      regexTime.lastIndex++;
     }
-    let hour = Number(n[1]);
-    let minute = Number(n[2]);
-    if(hour < 10) {
+    var hour = Number(n[1]);
+    var minute = Number(n[2]);
+    if (hour < 10) {
       hour = "0" + hour;
     } else {
       hour = hour.toString();
     }
-    if(minute < 10) {
+    if (minute < 10) {
       minute = "0" + minute;
     } else {
       minute = minute.toString();
