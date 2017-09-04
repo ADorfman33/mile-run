2fdc42fa68832458664e270825ec93536336fbf2
Andrew Dorfman <ADorfman@udel.edun>
2017-09-04 13:48:09 -0400

    adding floating buttons

---
+++ js/formating.js
@@ -0,0 +1,9 @@
+var $floatingBtn = $('.btn-floating');
+$(document).scroll(function() {
+  if($(this).scrollTop()>50){
+    $floatingBtn.slideDown('slow')
+  }
+  else{
+    $floatingBtn.slideUp('slow')
+  }
+});
